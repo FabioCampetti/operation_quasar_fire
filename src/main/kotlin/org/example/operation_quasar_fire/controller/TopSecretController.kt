e2db6eff31da5d3cb1d4f2370fe5ffe3aed7f5be
FabioCampetti <fabicampetti98@gmail.com>
2024-04-15 02:39:00 -0300

    Update swagger doc

--- src/main/kotlin/org/example/operation_quasar_fire/controller/TopSecretController.kt
+++ src/main/kotlin/org/example/operation_quasar_fire/controller/TopSecretController.kt
@@ -2,6 +2,8 @@ package org.example.operation_quasar_fire.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.example.operation_quasar_fire.dto.CarrierDTO
@@ -16,9 +18,18 @@ class TopSecretController : OperationQuasarFireController() {
 
     @Operation(summary = "Get the carrier top secret location from satellite collection")
     @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "Carrier position and message retrieved"),
-        ApiResponse(responseCode = "400", description = "Invalid data supplied"),
-        ApiResponse(responseCode = "500", description = "Internal error server")
+        ApiResponse(responseCode = "200", description = "Carrier position and message retrieved",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "200", value = "{\n  \"latitude\": 40.7128,\n  \"longitude\": -74.0060,\n  \"message\": \"We are safe\"\n}")])]),
+        ApiResponse(responseCode = "400", description = "Invalid data supplied",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "400", value = "{\n  \"statusCode\": 400,\n  \"message\": \"Invalid data\"\n}")])]),
+        ApiResponse(responseCode = "404", description = "Resource not found",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "404", value = "{\n  \"statusCode\": 404,\n  \"message\": \"Resource not found\"\n}")])]),
+        ApiResponse(responseCode = "500", description = "Internal server error",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "500", value = "{\n  \"statusCode\": 500,\n  \"message\": \"Internal server error\"\n}")])])
     ])
     @PostMapping("/topsecret")
     fun getTopSecretLocation(
--- src/main/kotlin/org/example/operation_quasar_fire/controller/TopSecretSplitController.kt
+++ src/main/kotlin/org/example/operation_quasar_fire/controller/TopSecretSplitController.kt
@@ -2,6 +2,8 @@ package org.example.operation_quasar_fire.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.example.operation_quasar_fire.dto.CarrierDTO
@@ -21,9 +23,15 @@ class TopSecretSplitController : OperationQuasarFireController() {
     @Operation(summary = "Add information about a satellite")
     @PostMapping("/topsecret_split/{satelliteName}")
     @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "Satellite information added"),
-        ApiResponse(responseCode = "400", description = "Invalid data supplied"),
-        ApiResponse(responseCode = "500", description = "Internal error server")
+        ApiResponse(responseCode = "200", description = "Satellite information added",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "200", value = "{\n  \"message\": \"Satellite information added successfully\"\n}")])]),
+        ApiResponse(responseCode = "400", description = "Invalid data supplied",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "400", value = "{\n  \"statusCode\": 400,\n  \"message\": \"Invalid data\"\n}")])]),
+        ApiResponse(responseCode = "500", description = "Internal server error",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "500", value = "{\n  \"statusCode\": 500,\n  \"message\": \"Internal server error\"\n}")])])
     ])
     fun addSatelliteInfo(
         @Parameter(description = "Name of the satellite", required = true) @PathVariable satelliteName: String,
@@ -35,9 +43,15 @@ class TopSecretSplitController : OperationQuasarFireController() {
 
     @Operation(summary ="Get carrier position and message")
     @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "Carrier position and message retrieved"),
-        ApiResponse(responseCode = "404", description = "No data found"),
-        ApiResponse(responseCode = "500", description = "Internal error server")
+        ApiResponse(responseCode = "200", description = "Carrier position and message retrieved",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "200", value = "{\n  \"latitude\": 40.7128,\n  \"longitude\": -74.0060,\n  \"message\": \"We are safe\"\n}")])]),
+        ApiResponse(responseCode = "404", description = "No data found",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "404", value = "{\n  \"statusCode\": 404,\n  \"message\": \"No data found\"\n}")])]),
+        ApiResponse(responseCode = "500", description = "Internal server error",
+            content = [Content(mediaType = "application/json",
+                examples = [ExampleObject(name = "500", value = "{\n  \"statusCode\": 500,\n  \"message\": \"Internal server error\"\n}")])])
     ])
     @GetMapping("/topsecret_split")
     fun getTopSecretSplit(): ResponseEntity<CarrierDTO> {
